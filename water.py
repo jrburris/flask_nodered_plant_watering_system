9cc293a8d52f4f5182143d31539c48d43822c12f
jrburris <jrburris66@mac.com>
2020-11-28 11:36:48 -0500

    Code cleanup

--- water.py
+++ water.py
@@ -1,14 +1,7 @@
 # External module imp
 import datetime
 import time
-
 import sys
-# import fake_rpi
-
-# sys.modules['RPi'] = fake_rpi.RPi     # Fake RPi
-# sys.modules['RPi.GPIO'] = fake_rpi.RPi.GPIO # Fake GPIO
-# # sys.modules['smbus'] = fake_rpi.smbus # Fake smbus (I2C)
-
 import RPi.GPIO as GPIO
 
 
